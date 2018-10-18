f614cc9cf3a3a6b2a0b5c9e061dbc71d9962be08
Lokesh Tiwari <33743030+bitlucky@users.noreply.github.com>
2018-10-18 19:25:31 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+var app_fireBase= {} ;
+  (function(){
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA6GoVHaXb8JQojrv_a7xkO7sGLONQR-VY",
+    authDomain: "web-handle.firebaseapp.com",
+    databaseURL: "https://web-handle.firebaseio.com",
+    projectId: "web-handle",
+    storageBucket: "web-handle.appspot.com",
+    messagingSenderId: "200667003861"
+  };
+  firebase.initializeApp(config);
+
+app_fireBase = firebase ;
+})()
